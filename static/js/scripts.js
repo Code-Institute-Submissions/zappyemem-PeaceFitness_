d8a9a4fda9f65d14c5dce8732af12d1b22a59e45
Emem Simon <zappy4chat@yahoo.com>
2021-09-21 13:02:48 +0000

    added categories functionality on app.py

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,1 +1,21 @@
 $('.datepicker').datepicker();
+
+
+function disableActions(bDisable = "none") {
+	let cardActions = document.getElementsByClassName("card-action");
+	for(let i = 0; i < cardActions.length; i++) {
+		cardActions[i].style.pointerEvents = bDisable
+	}
+}
+
+function openModal() {
+	let url = $('#confirm_modal_button').data('url');
+	$('#yes_confirm').attr('href', url);
+	$("#confirm_modal").show();
+	disableActions();
+}
+
+function hideModal() {
+	$('#confirm_modal').hide();
+	disableActions("auto");
+}
